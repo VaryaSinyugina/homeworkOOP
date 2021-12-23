44e18352442b0b3c3af779ed98a1f8e3aabb2481
Varya Sinyugina <varya.sinyugina@mail.com>
2021-12-23 21:27:25 +0900

    All files were added

---
+++ lab1/лаба1/lab 1.cpp
@@ -0,0 +1,23 @@
+﻿// лаба1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+ 
+using namespace std;
+
+int main()
+{
+    int x, y;
+    float arf, geom;
+
+    cout << " x = ";
+    cin >> x;
+    cout << " y = ";
+    cin >> y;
+
+    arf = (x + y) / 2;
+    geom = sqrt(x * y);
+}
+
+
